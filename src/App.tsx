a3619bd1bc09d5480b91e1a76672a36ec25f804d
faddishcorn <juni129461@gmail.com>
2024-09-30 01:27:55 +0900

    chore: 임시파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TempPage from './pages/TempPage';
+import "./App.css";
 import 'normalize.css';
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TempPage />} />
-        {/* route추가예정... */}
-      </Routes>
-    </Router>
-  );
+  return <></>;
 }
 
 export default App
