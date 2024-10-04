7a36cceddb6f901b23a6cceaafb93bd105a8dfea
faddishcorn <juni129461@gmail.com>
2024-10-04 18:52:59 +0900

    chore: router및 page기본세팅

---
+++ src/pages/Mypage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyPage: React.FC = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+      <p>This is your profile page.</p>
+    </div>
+  );
+};
+
+export default MyPage;
---
+++ src/router/routerPath.ts
@@ -0,0 +1,10 @@
+export const RouterPath = {
+  home: "/",
+  main: "/main",
+  login: "/login",
+  previewPlan: "/plan/preview",
+  teamPlan: "/team-plan",
+  plan: "/plan",
+  friend: "/friend",
+  myPage: "/mypage",
+};
