d88f17023d4d5c1d1aea322d18ab09a76f062684
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-10-11 20:17:31 +0900

    feature: 랜딩 페이지 구현

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -2,37 +2,71 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 import circleSVG from "@/assets/circle (1).svg"; // 원형 이미지
-import mockSVG from "@/assets/mock.svg"; // 목업 이미지
+import mockSVG from "@/assets/mock2.svg"; // 목업 이미지
+import effectSVG from "@/assets/effect.svg";
+import logoSVG from "@/assets/logo.svg";
+import HowToUse from "@/components/features/HowToUse/HowToUse";
+import Button from "@/components/common/Button/Button";
 
 // 스타일 정의
-const LandingContainer = styled.div`
-  position: relative;
+const NavBar = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: flex-start; /* 콘텐츠를 가운데 정렬 */
-  align-items: center;
-  background-color: #f9f9f9;
-`;
-
-const ContentWrapper = styled.div`
+  height: 120px;
+  background-color: white;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  max-width: 1280px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  z-index: 100;
+`;
+
+const Logo = styled.img`
+  height: 70px;
+`;
+const NaviBarLink = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+const NavLink = styled.a`
+  text-decoration: none;
+  color: #333;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: 600;
+  &:hover {
+    color: #39a7f7;
+  }
+`;
+
+const LandingContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 120px;
+`;
+
+const ContentWrapper = styled.div`
   width: 100%;
-  padding-left: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   z-index: 1;
   position: relative;
+  margin: 0 auto;
 `;
 
 const BackgroundCircle = styled.img`
   position: absolute;
-  left: -350px; /* 화면 가장자리와 붙음 */
-  bottom: 0px; /* 하단 가장자리와 붙음 */
-  width: 400px; /* 원의 크기 조정 */
-  height: 800px;
-  z-index: 0; /* 텍스트 뒤로 보내기 */
+  left: -360px;
+  bottom: -100px;
+  width: 400px;
+  height: auto;
+  z-index: 0;
   opacity: 0.5;
 `;
 
@@ -40,19 +74,51 @@ const TextWrapper = styled.div`
   max-width: 600px;
   margin-right: 20px;
   margin-left: -150px;
+  margin-top: -200px;
+`;
+
+const TitleWithImage = styled.div`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  margin-left: -100px;
+  margin-top: -60px;
 `;
 
 const Title = styled.h1`
-  font-size: 48px;
+  font-size: 50px;
   font-weight: bold;
-  color: #333;
-  margin-bottom: 20px;
+  color: #39a7f7;
+  margin-bottom: 0px;
 `;
 
 const SubTitle = styled.p`
-  font-size: 24px;
-  color: #666;
+  font-size: 66px;
+  font-weight: bold;
+  color: #39a7f7;
+  margin-top: -20px;
+  margin-bottom: 0px;
+  margin-left: -70px;
+`;
+
+const AdditionalText = styled.p`
+  font-size: 40px;
+  font-weight: bold;
+  color: #3f3f3f;
+  margin-top: 20px;
   margin-bottom: 40px;
+  margin-left: -80px;
+`;
+
+const HighlightText = styled.p`
+  color: #39a7f7;
+  display: inline;
+`;
+
+const EffectImage = styled.img`
+width: 50px,
+height: 63px;
+margin-left: 10px
 `;
 
 const ButtonContainer = styled.div`
@@ -60,25 +126,10 @@ const ButtonContainer = styled.div`
   gap: 20px;
 `;
 
-const Button = styled.button`
-  padding: 15px 30px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 18px;
-  color: #fff;
-`;
-
-const LoginButton = styled(Button)`
-  background-color: #39a7f7;
-`;
-
-const StartButton = styled(Button)`
-  background-color: #007aff;
-`;
-
 const MockupImage = styled.img`
-  width: 500px;
+  width: 700px;
+  margin-top: 50px;
+  margin-left: -50px;
 `;
 
 const LandingPage: React.FC = () => {
@@ -93,20 +144,40 @@ const LandingPage: React.FC = () => {
   };
 
   return (
-    <LandingContainer>
-      <ContentWrapper>
-        <TextWrapper>
-          <Title>간편하고 빠른 플랜 생성</Title>
-          <SubTitle>내 목소리로 일정을 만들고, 쉽게 관리하세요.</SubTitle>
-          <ButtonContainer>
-            <LoginButton onClick={handleLoginClick}>로그인</LoginButton>
-            <StartButton onClick={handleStartClick}>시작하기</StartButton>
-          </ButtonContainer>
-        </TextWrapper>
-        <MockupImage src={mockSVG} alt="목업 이미지" />
-      </ContentWrapper>
-      <BackgroundCircle src={circleSVG} alt="배경 원" />
-    </LandingContainer>
+    <>
+      <NavBar>
+        <Logo src={logoSVG} alt="로고" />
+        <NaviBarLink>
+          <NavLink href="#introduce">소개</NavLink>
+          <NavLink href="#pricing">사용 방법</NavLink>
+          <NavLink href="#subscribe">구독하기</NavLink>
+          <NavLink href="#free-trial">무료 체험</NavLink>
+        </NaviBarLink>
+      </NavBar>
+
+      <LandingContainer>
+        <ContentWrapper>
+          <TextWrapper>
+            <TitleWithImage>
+              <Title>간편하고 빠른 플랜 생성</Title>
+              <EffectImage src={effectSVG} alt="효과 이미지" />
+            </TitleWithImage>
+            <SubTitle>Speak, and Plan it!</SubTitle>
+            <AdditionalText>
+              <HighlightText>내 목소리</HighlightText>로 일정을 만들고, <br />
+              쉽게 관리해보세요!
+            </AdditionalText>
+            <ButtonContainer>
+              <Button onClick={handleLoginClick}>로그인</Button>
+              <Button onClick={handleStartClick}>시작하기</Button>
+            </ButtonContainer>
+          </TextWrapper>
+          <MockupImage src={mockSVG} alt="목업 이미지" />
+          <BackgroundCircle src={circleSVG} alt="배경 원" />
+        </ContentWrapper>
+      </LandingContainer>
+      <HowToUse />
+    </>
   );
 };
 
