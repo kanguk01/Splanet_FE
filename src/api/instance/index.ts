ee691d1681ed0666ef674d2bf7d8a08204d07a06
faddishcorn <juni129461@gmail.com>
2024-10-11 19:01:03 +0900

    feat: apiClient 설정

---
+++ src/api/instance/index.ts
@@ -0,0 +1,17 @@
+// src/api/instance/index.ts
+import axios from 'axios';
+import { QueryClient } from '@tanstack/react-query';
+
+// Axios 인스턴스 생성
+// axios.create()를 통해 기본 설정
+export const apiClient = axios.create({
+  baseURL: 'https://example.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const apiBaseUrl = apiClient.defaults.baseURL; //msw에서 쓸려고
+
+// QueryClient 생성 (전역적으로 사용할 수 있는 데이터 관리 객체)
+export const queryClient = new QueryClient();
