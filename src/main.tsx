773016397348025139b659fb31fc3ab8b7eaf92f
faddishcorn <juni129461@gmail.com>
2024-10-11 18:58:23 +0900

    chore: msw설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// createRoot전에 mockging api 먼저 실행
+async function enableMocking() {
+  // process.env.NODE_ENV
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('@/api/mocks/browser.ts');  // 서비스 워커 임포트
+
+  return worker.start();  // 서비스 워커 시작
+}
+
+enableMocking().then(() => {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+})
+
