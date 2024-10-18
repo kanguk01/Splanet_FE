cf64b398f625178326544103005e7f0fa5e1acde
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-10-18 21:57:07 +0900

    feat: 숫자 버튼 컴포넌트 생성

---
+++ src/components/common/NumberButton/NumberButton.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import breakpoints from "@/variants/breakpoints";
+
+const StyledNumberButton = styled.button`
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border: #39a7f7;
+  border-radius: 16x;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  color: #39a7f7;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #39a7f7;
+    border: none;
+    color: white;
+  }
+
+  //반응형 미디어 쿼리 추가
+  ${breakpoints.tablet} {
+    width: 60px;
+    height: 60px;
+    font-size: 20px;
+  }
+
+  ${breakpoints.desktop} {
+  width: 70px;
+  height: 70px;
+  font-sizeL 20px;
+  }
+`;
+
+interface Props {
+  number: number;
+}
+
+const NumberButton = ({ number }: Props) => {
+  return <StyledNumberButton>{number}</StyledNumberButton>;
+};
+
+export default NumberButton;
