efa8313d27ba50afde6b37b6d42a96d04a3f2155
faddishcorn <juni129461@gmail.com>
2024-10-22 23:36:02 +0900

    chore: input 스타일 변경

--- src/components/common/Input/Input.styles.ts
+++ src/components/common/Input/Input.styles.ts
@@ -1,29 +1,28 @@
+// Input.styles.ts
 import styled from "@emotion/styled";
 import breakpoints from "@/variants/variants";
 
-const StyledInput = styled.input(
-  {
-    borderRadius: "16px",
-    padding: "10px",
-    border: "3px solid #D5D5D5",
-    fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
-    fontWeight: 600,
-    resize: "none",
-    width: "calc(100% - 60px)",
-    margin: "0 30px",
-  },
-  {
-    height: "187px",
-    fontSize: "16px",
-    [breakpoints.tablet]: {
-      height: "450px",
-      fontSize: "26px",
-    },
-    [breakpoints.desktop]: {
-      height: "392px",
-      fontSize: "32px",
-    },
-  },
-);
+const StyledInput = styled.textarea`
+  border-radius: 16px;
+  padding: 10px;
+  border: 3px solid #d5d5d5;
+  font-family: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif";
+  font-weight: 600;
+  width: 80%;
+  margin: 0 30px;
+  text-align: left;
+  vertical-align: top;
+  max-width: 100%;
+  max-height: 300px;
+  height: 450px;
+  font-size: 20px;
+  resize: none;
+
+  // 모바일
+  @media (max-width: 768px) {
+    height: 250px;
+    font-size: 16px;
+  }
+`;
 
 export default StyledInput;
--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import StyledInput from "./Input.styles";
 
 export type Props = {
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-  value: string;
+  onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
+  value?: string;
   placeholder?: string;
   disabled?: boolean;
 };
 
-const Input: React.FC<Props> = ({ onChange, value, placeholder, disabled }) => {
+const Input: React.FC<Props> = ({ value, placeholder, disabled, onChange }) => {
   return (
     <StyledInput
-      onChange={onChange}
+    onChange={onChange}
       value={value}
       placeholder={placeholder}
       disabled={disabled}
