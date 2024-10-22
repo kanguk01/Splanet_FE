b78be8760b1a5c1386ac095427d15cc53d1d9afe
faddishcorn <juni129461@gmail.com>
2024-10-22 23:36:50 +0900

    chore: landingpage 媛쒖꽑

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
-import circleSVG from "@/assets/circle (1).svg"; // 원형 이미지
-import mockSVG from "@/assets/mock2.svg"; // 목업 이미지
+import { useNavigate } from "react-router-dom";
+import breakpoints from "@/variants/variants";
+import circleSVG from "@/assets/circle (1).svg";
+import mockSVG from "@/assets/mock2.svg";
 import effectSVG from "@/assets/effect.svg";
 import logoSVG from "@/assets/logo.svg";
 import HowToUse from "@/components/features/HowToUse/HowToUse";
 import Button from "@/components/common/Button/Button";
 
 // 스타일 정의
-const NavBar = styled.div`
+export const NavBar = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 120px;
+  height: 100px;
   background-color: white;
   display: flex;
   align-items: center;
@@ -22,114 +22,170 @@ const NavBar = styled.div`
   padding: 0 20px;
   box-sizing: border-box;
   z-index: 100;
+
+  ${breakpoints.tablet} {
+    height: 120px;
+    padding: 0 40px;
+  }
 `;
 
 const Logo = styled.img`
-  height: 70px;
+  height: 50px;
+
+  ${breakpoints.tablet} {
+    height: 70px;
+  }
 `;
+
 const NaviBarLink = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 10px;
+
+  ${breakpoints.tablet} {
+    gap: 20px;
+  }
 `;
+
 const NavLink = styled.a`
   text-decoration: none;
   color: #333;
-  font-size: 15px;
+  font-size: clamp(12px, 2vw, 15px); 
   cursor: pointer;
   font-weight: 600;
+
   &:hover {
     color: #39a7f7;
   }
 `;
 
 const LandingContainer = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 120px;
+  padding: 120px 0px 40px;
+  overflow-x: hidden;
+  ${breakpoints.tablet} {
+    padding: 140px 40px 60px;
+  }
 `;
 
 const ContentWrapper = styled.div`
   width: 100%;
+ 
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  z-index: 1;
   position: relative;
-  margin: 0 auto;
+
+  ${breakpoints.tablet} {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const BackgroundCircle = styled.img`
-  position: absolute;
-  left: -360px;
-  bottom: -100px;
-  width: 400px;
-  height: auto;
-  z-index: 0;
-  opacity: 0.5;
+  display: none;
+
+  ${breakpoints.tablet} {
+    display: block;
+    position: absolute;
+    left: -200px;
+    bottom: -100px;
+    width: 400px;
+    z-index: -1;
+    opacity: 0.5;
+  }
 `;
 
 const TextWrapper = styled.div`
-  max-width: 600px;
-  margin-right: 20px;
-  margin-left: -150px;
-  margin-top: -200px;
+  width: 100%;
+  max-width: 500px;
+  text-align: center;
+  margin-bottom: 40px;
+
+  ${breakpoints.tablet} {
+    width: 45%;
+    text-align: left;
+    margin-bottom: 0;
+    margin-right: 5%;
+  }
 `;
 
 const TitleWithImage = styled.div`
   display: flex;
   align-items: center;
-  white-space: nowrap;
-  margin-left: -100px;
-  margin-top: -60px;
+  justify-content: center;
+  margin-bottom: 10px;
+
+  ${breakpoints.tablet} {
+    justify-content: flex-start;
+  }
 `;
 
 const Title = styled.h1`
-  font-size: 50px;
+  font-size: clamp(28px, 5vw, 48px);
   font-weight: bold;
   color: #39a7f7;
-  margin-bottom: 0px;
+  white-space: nowrap;
 `;
 
 const SubTitle = styled.p`
-  font-size: 66px;
+  font-size: clamp(24px, 4vw, 40px);
   font-weight: bold;
   color: #39a7f7;
-  margin-top: -20px;
-  margin-bottom: 0px;
-  margin-left: -70px;
+  margin-top: 10px;
 `;
 
 const AdditionalText = styled.p`
-  font-size: 40px;
+  font-size: clamp(18px, 3vw, 30px);
   font-weight: bold;
   color: #3f3f3f;
   margin-top: 20px;
-  margin-bottom: 40px;
-  margin-left: -80px;
 `;
 
-const HighlightText = styled.p`
+const HighlightText = styled.span`
   color: #39a7f7;
-  display: inline;
 `;
 
 const EffectImage = styled.img`
-width: 50px,
-height: 63px;
-margin-left: 10px
+  width: clamp(30px, 5vw, 50px);
+  height: auto;
+  margin-left: 10px;
 `;
 
 const ButtonContainer = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 10px;
+  justify-content: center;
+  margin-top: 20px;
+
+  ${breakpoints.tablet} {
+    justify-content: flex-start;
+  }
+`;
+
+const MockupImageWrapper = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  ${breakpoints.tablet} {
+    width: 50%;
+    justify-content: flex-end;
+    padding-top: 150px;
+  }
 `;
 
 const MockupImage = styled.img`
-  width: 700px;
-  margin-top: 50px;
-  margin-left: -50px;
+  width: 100%;
+  max-width: 450px;
+  height: auto;
+  object-fit: contain;
 `;
 
 const LandingPage: React.FC = () => {
@@ -164,15 +220,17 @@ const LandingPage: React.FC = () => {
             </TitleWithImage>
             <SubTitle>Speak, and Plan it!</SubTitle>
             <AdditionalText>
-              <HighlightText>내 목소리</HighlightText>로 일정을 만들고, <br />
-              쉽게 관리해보세요!
+              <HighlightText>내 목소리</HighlightText>로 일정을 만들고,
+              <br /> 쉽게 관리해보세요!
             </AdditionalText>
             <ButtonContainer>
               <Button onClick={handleLoginClick}>로그인</Button>
               <Button onClick={handleStartClick}>시작하기</Button>
             </ButtonContainer>
           </TextWrapper>
-          <MockupImage src={mockSVG} alt="목업 이미지" />
+          <MockupImageWrapper>
+            <MockupImage src={mockSVG} alt="목업 이미지" />
+          </MockupImageWrapper>
           <BackgroundCircle src={circleSVG} alt="배경 원" />
         </ContentWrapper>
       </LandingContainer>
