708e8ced38f92e113f1ffe269fd28877a16d635c
faddishcorn <juni129461@gmail.com>
2024-10-22 23:31:06 +0900

    chore: mock 설정

--- src/api/mocks/browser.ts
+++ src/api/mocks/browser.ts
@@ -1,7 +1,11 @@
 // src/mocks/browser.ts
 import { setupWorker } from "msw/browser";
 import friendsMockHandlers from "./friends.mock";
+import loginMockHandler from "./login.mock";
 
-const worker = setupWorker(...friendsMockHandlers);
+const worker = setupWorker(
+  ...friendsMockHandlers,
+  ...loginMockHandler,
+);
 
 export default worker;
--- src/api/mocks/plans.mock.ts
+++ src/api/mocks/plans.mock.ts
@@ -1,1 +1,2 @@
-//
+import { http, HttpResponse } from "msw"; // HttpResponse 추가
+import { apiBaseUrl } from "../instance";
