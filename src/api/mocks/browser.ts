8ad0268cf3644b1b61e7ffa00e98b09b62560b09
faddishcorn <juni129461@gmail.com>
2024-10-25 11:59:27 +0900

    chore: msw handler설정

--- src/api/mocks/browser.ts
+++ src/api/mocks/browser.ts
@@ -2,10 +2,16 @@
 import { setupWorker } from "msw/browser";
 import friendsMockHandlers from "./friends.mock";
 import loginMockHandler from "./login.mock";
+import plansMockHandlers from "./plans.mock";
+import paymentMockHandlers from "./payments.mock";
+import userMockHandlers from "./users.mock";
 
 const worker = setupWorker(
   ...friendsMockHandlers,
   ...loginMockHandler,
+  ...plansMockHandlers,
+  ...paymentMockHandlers,
+  ...userMockHandlers,
 );
 
 export default worker;
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,7 +2,7 @@ export interface Friend {
   friend_id: number;
   friend_name: string;
   friend_profile_image: string;
-  created_at: string;
+  createdAt: string;
 }
 
 export interface SentRequest {
