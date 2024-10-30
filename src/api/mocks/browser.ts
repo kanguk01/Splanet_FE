07645cc9ff8f8f46af33db81d8de400387e9f81a
faddishcorn <juni129461@gmail.com>
2024-10-30 13:38:50 +0900

    chore: mocking 비활성화

--- src/api/mocks/browser.ts
+++ src/api/mocks/browser.ts
@@ -1,17 +1,19 @@
-// src/mocks/browser.ts
-import { setupWorker } from "msw/browser";
-import friendsMockHandlers from "./friends.mock";
-import loginMockHandler from "./login.mock";
-import plansMockHandlers from "./plans.mock";
-import paymentMockHandlers from "./payments.mock";
-import userMockHandlers from "./users.mock";
+// // src/mocks/browser.ts
+// import { setupWorker } from "msw/browser";
+// import friendsMockHandlers from "./friends.mock";
+// import loginMockHandler from "./login.mock";
+// import plansMockHandlers from "./plans.mock";
+// import paymentMockHandlers from "./payments.mock";
+// import userMockHandlers from "./users.mock";
+// import commentsMockHandlers from "./comments.mock";
 
-const worker = setupWorker(
-  ...friendsMockHandlers,
-  ...loginMockHandler,
-  ...plansMockHandlers,
-  ...paymentMockHandlers,
-  ...userMockHandlers,
-);
+// const worker = setupWorker(
+//   ...friendsMockHandlers,
+//   // ...loginMockHandler,
+//   ...plansMockHandlers,
+//   ...paymentMockHandlers,
+//   ...userMockHandlers,
+//   ...commentsMockHandlers,
+// );
 
-export default worker;
+// export default worker;
