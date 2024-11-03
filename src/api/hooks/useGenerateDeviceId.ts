4184f09fb1eaaf0016ea2904eeb02f6095b8ec0e
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2024-11-04 03:12:19 +0900

    feat: DeviceId 가져오는 api hooks 생성

---
+++ src/api/hooks/useGenerateDeviceId.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { apiClient } from "@/api/instance";
+
+const setDeviceIdCookie = (deviceId: string) => {
+  document.cookie = `device_id=${deviceId}; path=/; Secure; SameSite=Strict`;
+};
+
+const fetchDeviceId = async () => {
+  const response = await apiClient.get(`/api/gpt/generate-device-id`);
+  const deviceId = response.data;
+
+  // deviceId를 쿠키에 저장
+  setDeviceIdCookie(deviceId);
+
+  return deviceId;
+};
+
+const useGenerateDeviceId = () => {
+  return useQuery({
+    queryKey: ["deviceId"],
+    queryFn: fetchDeviceId,
+  });
+};
+
+export default useGenerateDeviceId;
