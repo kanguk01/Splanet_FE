5fee55473c1a3e1bfc9c543bcb3085945038375d
faddishcorn <juni129461@gmail.com>
2024-11-06 15:46:27 +0900

    chore: type추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -25,3 +25,47 @@ export interface SearchResult {
   nickname: string;
   profileImage: string;
 }
+
+export interface Member {
+  id: number;
+  nickname: string;
+}
+
+export interface Team {
+  id: number;
+  teamName: string;
+  user: Member; // 관리자 정보
+  teamMembers: Member[]; // 팀 멤버 배열
+}
+
+export interface Invitation {
+  invitationId: number;
+  teamId: number;
+  teamName: string;
+  userId: number;
+  nickname: string;
+  profileImage: string;
+  status: string; // "PENDING" 등의 상태값
+}
+
+export interface InviteUserParams {
+  teamId: number;
+  nickname: string;
+}
+
+export interface TeamMember {
+  userId: number;
+  nickname: string;
+  profileImage: string | null;
+  role: "ADMIN" | "MEMBER";
+}
+
+export interface SentInvitation {
+  invitationId: number;
+  teamId: number;
+  teamName: string;
+  userId: number;
+  nickname: string;
+  profileImage: string | null;
+  status: "PENDING";
+}
