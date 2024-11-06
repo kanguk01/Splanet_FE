a2393e35cbc88a0dedc659d4fd94e415d7c81ddb
faddishcorn <juni129461@gmail.com>
2024-11-06 17:24:00 +0900

    chore: routerPath 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -22,8 +22,11 @@ import PreviewPlanSelectPage from "@/pages/PreviewPlan/PreviewPlanSelectPage";
 import PreviewPlanUpdate from "@/pages/PreviewPlan/PreviewPlanUpdate";
 import PreviewPlanPage from "@/pages/PreviewPlan/PreviewPlanPage";
 import TeamPlanMakingPage from "@/pages/TeamPlan/TeamPlanMaking";
-import TeamPlanUpdate from "@/pages/TeamPlan/TeamPlanUpdatePage";
+import TeamPlanUpdate from "@/pages/TeamPlan/TeamPlanUpdate";
 import OAuthRedirectHandler from "@/pages/LoginModal/RedirectPage";
+import TeamPlanSelectPage from "@/pages/TeamPlan/TeamPlanSelectPage";
+import TeamInvitePage from "@/pages/TeamPlan/TeamPlanInvite";
+import TeamPlanChangePage from "@/pages/TeamPlan/TeamPlanChange";
 
 function Router() {
   const { authState } = useAuth();
@@ -64,7 +67,7 @@ function Router() {
         { path: RouterPath.MAIN, element: <MainPage /> },
         { path: RouterPath.TEAM_PLAN, element: <TeamPlan /> },
         {
-          path: `${RouterPath.TEAM_PLAN}/:teamId`,
+          path: `${RouterPath.TEAM_PLAN_DETAIL}`,
           element: <TeamPlanDetailPage />,
         },
         { path: RouterPath.MY_PAGE, element: <MyPage /> },
@@ -81,7 +84,10 @@ function Router() {
         { path: RouterPath.PLAN_SELECT, element: <PlanSelectPage /> },
         { path: RouterPath.PLAN_UPDATE, element: <PlanUpdate /> },
         { path: RouterPath.TEAM_PLAN_MAKING, element: <TeamPlanMakingPage /> },
+        { path: RouterPath.TEAM_PLAN_SELECT, element: <TeamPlanSelectPage/>},
         { path: RouterPath.TEAM_PLAN_UPDATE, element: <TeamPlanUpdate /> },
+        {path: RouterPath.TEAM_PLAN_INVITE, element: <TeamInvitePage/>},
+        {path: RouterPath.TEAM_PLAN_CHANGE, element: <TeamPlanChangePage/>}
       ],
     },
   ]);
