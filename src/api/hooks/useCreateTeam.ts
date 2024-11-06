9743bc63d74ec8127ba9c3f166e880e5b37058c8
faddishcorn <juni129461@gmail.com>
2024-11-06 15:46:53 +0900

    feat: team 생성 hook

---
+++ src/api/hooks/useCreateTeam.ts
@@ -0,0 +1,25 @@
+import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import { apiClient } from "@/api/instance";
+
+interface CreateTeamResponse {
+  id: number;
+  teamName: string;
+  user: {
+    id: number;
+    nickname: string;
+  };
+  teamMembers: Array<{ id: number; nickname: string }>;
+}
+
+export const useCreateTeam = (
+  options?: UseMutationOptions<AxiosResponse<CreateTeamResponse>, Error, string>
+) => {
+  return useMutation<AxiosResponse<CreateTeamResponse>, Error, string>({
+    mutationFn: (teamName) =>
+      apiClient.post<CreateTeamResponse>("/api/teams", null, {
+        params: { teamName },
+      }),
+    ...options,
+  });
+};
