dd854823287dbad84dd696ab3da359f609b9a661
faddishcorn <juni129461@gmail.com>
2024-11-10 16:22:30 +0900

    chore: numberbutton 스타일 변경

--- src/components/common/NumberButton/NumberButton.tsx
+++ src/components/common/NumberButton/NumberButton.tsx
@@ -1,33 +1,34 @@
+// src/components/common/NumberButton/NumberButton.tsx
 import styled from "@emotion/styled";
 import breakpoints from "@/variants/breakpoints";
 
 const StyledNumberButton = styled.button<{ clicked: boolean }>`
-  width: 70px;
-  height: 70px;
-  background-color: ${({ clicked }) => (clicked ? "#39a7f7" : "#fff")};
-  border: 2px solid #39a7f7;
-  border-radius: 16px;
+  width: 60px;
+  height: 60px;
+  background-color: ${({ clicked }) => (clicked ? "#39a7f7" : "#f0f4fa")};
+  border: none;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   color: ${({ clicked }) => (clicked ? "#fff" : "#39a7f7")};
   font-size: 20px;
   font-weight: bold;
   cursor: pointer;
   outline: none;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
 
   &:hover {
     background-color: #39a7f7;
-    border: none;
     color: white;
   }
 
   ${breakpoints.tablet} {
     width: 50px;
     height: 50px;
-    font-size: 20px;
-    border-radius: 16px;
+    font-size: 18px;
   }
 `;
 
