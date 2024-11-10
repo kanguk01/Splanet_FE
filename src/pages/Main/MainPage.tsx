63742ea58cf7650130cd944675bf1979636c0c6c
faddishcorn <juni129461@gmail.com>
2024-11-10 16:29:48 +0900

    feat: sessionStorage에 저장된 plan 저장 후 삭제

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,289 +1,87 @@
+// mainPage.tsx
+import { useEffect, useRef } from "react";
 import styled from "@emotion/styled";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import CustomCalendar from "@/components/features/CustomCalendar/CustomCalendar";
+import { useNavigate } from "react-router-dom";
+import CustomCalendar, {
+  CalendarEvent,
+} from "@/components/features/CustomCalendar/CustomCalendar";
 import { useGetPlans } from "@/api/hooks/useGetPlans";
-import useCreatePlan from "@/api/hooks/useCreatePlans";
-import useDeletePlan from "@/api/hooks/useDeletePlans";
-import CircleButton from "@/components/common/CircleButton/CircleButton";
-import breakpoints from "@/variants/breakpoints";
-import useUpdatePlan from "@/api/hooks/useUpdatePlan";
+import useCreatePlan from "@/api/hooks/useCreatePlan";
+import Button from "@/components/common/Button/Button";
+import RouterPath from "@/router/RouterPath";
 
-// 스타일드 컴포넌트 정의
-const CalendarContainer = styled.div`
-  position: relative;
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
+const PageContainer = styled.div`
+  background-color: #ffffff;
   padding: 20px;
 `;
 
-const ButtonWrapper = styled.div`
-  position: absolute;
-  top: 10%;
-  display: flex;
-  justify-content: flex-start;
-  margin-top: 20px;
-  left: calc(10% + 10px);
-  z-index: 1;
-  ${breakpoints.tablet} {
-    top: 10%;
-    left: 20px;
-  }
-  ${breakpoints.mobile} {
-    top: 10%;
-    left: 20px;
-  }
-`;
+export default function MainlPage() {
+  const { data: Plans, isLoading, error, refetch } = useGetPlans();
 
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 2;
-`;
-
-const ModalContent = styled.div`
-  background: white;
-  padding: 20px 30px;
-  width: 500px;
-  max-width: 95%;
-  border-radius: 8px;
-  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.2);
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 8px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
-const Textarea = styled.textarea`
-  width: 99%;
-  padding: 10px;
-  margin-top: 5px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  resize: vertical;
-`;
-
-const CloseButton = styled.button`
-  margin-top: 10px;
-  background-color: #39a7f7;
-  color: white;
-  border: none;
-  padding: 10px 15px;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-const ToggleWrapper = styled.label`
-  display: flex;
-  flex-direction: column;
-  cursor: pointer;
-  gap: 10px;
-`;
-
-const ToggleInput = styled.input`
-  appearance: none;
-  width: 40px;
-  height: 20px;
-  background: #ccc;
-  border-radius: 20px;
-  position: relative;
-  outline: none;
-  transition: background 0.3s;
-  &:checked {
-    background: #39a7f7;
-  }
-  &:before {
-    content: "";
-    position: absolute;
-    width: 18px;
-    height: 18px;
-    border-radius: 50%;
-    background: white;
-    top: 1px;
-    left: 1px;
-    transition: transform 0.3s;
-  }
-  &:checked:before {
-    transform: translateX(20px);
-  }
-`;
-
-const ToggleLabel = styled.span`
-  font-size: 16px;
-  color: #333;
-`;
-
-const usePlanData = (initialSavedPlan: any) => {
-  const { data: plans, isLoading, error } = useGetPlans();
-  const createPlanMutation = useCreatePlan();
-  const deletePlanMutation = useDeletePlan();
-  const updatePlanMutation = useUpdatePlan();
-  const [savedPlan, setSavedPlan] = useState(initialSavedPlan || []);
+  const savePlanMutation = useCreatePlan();
+  const isPlanSaved = useRef(false);
+  const hasMounted = useRef(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (Array.isArray(initialSavedPlan)) {
-      setSavedPlan(initialSavedPlan);
-    }
-  }, [initialSavedPlan]);
+    if (hasMounted.current) return; // strict모드로 인한 두 번 실행을 막기위해
+    hasMounted.current = true;
 
-  const combinedPlans = [...(plans || []), ...savedPlan];
+    const savePlans = async () => {
+      const storedPlans = sessionStorage.getItem("plans");
+      if (storedPlans && !isPlanSaved.current) {
+        const parsedPlans: CalendarEvent[] = JSON.parse(storedPlans).map(
+          (plan: CalendarEvent) => ({
+            ...plan,
+            start: new Date(plan.start),
+            end: new Date(plan.end),
+          }),
+        );
 
-  const createPlan = (newPlan: any) => {
-    createPlanMutation.mutate(newPlan);
+        try {
+          for (const plan of parsedPlans) {
+            await savePlanMutation.mutateAsync({
+              plan: {
+                title: plan.title,
+                description: plan.description,
+                startDate: plan.start.toISOString(),
+                endDate: plan.end.toISOString(),
+                accessibility: plan.accessibility ?? true,
+                isCompleted: plan.complete ?? false,
+              },
+            });
+          }
+          sessionStorage.removeItem("plans");
+          console.log("세션의 플랜이 저장되었습니다.");
+          isPlanSaved.current = true; // Set flag after saving
+          refetch();
+        } catch (error) {
+          console.error("세션의 플랜 저장 실패:", error);
+        }
+      }
+    };
+
+    savePlans();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleModifyClick = () => {
+    navigate(RouterPath.MAIN_MODIFY, { state: { plans: Plans } });
   };
 
-  const handleDeletePlan = (planId: string) => {
-    deletePlanMutation.mutate(Number(planId));
-  };
-
-  const handleUpdatePlan = (planId: string, updatedPlanData: any) => {
-    updatePlanMutation.mutate({
-      planId: Number(planId),
-      planData: updatedPlanData,
-    });
-  };
-
-  return {
-    plans: combinedPlans,
-    isLoading,
-    error,
-    createPlan,
-    handleDeletePlan,
-    handleUpdatePlan,
-  };
-};
-
-// MainPage 컴포넌트
-const MainPage: React.FC = () => {
-  const location = useLocation();
-  const {
-    plans,
-    isLoading,
-    error,
-    createPlan,
-    handleDeletePlan,
-    // handleUpdatePlan,
-  } = usePlanData(location.state?.savedPlan);
-
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [isAccessible, setIsAccessible] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
-
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    createPlan({
-      id: Date.now().toString(),
-      title,
-      description,
-      startDate,
-      endDate,
-      accessibility: isAccessible,
-      isCompleted,
-    });
-    closeModal();
-  };
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isLoading) return <p>로딩 중...</p>;
+  if (error) return <p>데이터를 불러오지 못했습니다. 오류: {error.message}</p>;
 
   return (
-    <CalendarContainer>
-      <ButtonWrapper>
-        <CircleButton onClick={openModal}>+</CircleButton>
-      </ButtonWrapper>
-      <CustomCalendar
-        plans={plans}
-        onDeletePlan={handleDeletePlan}
-        // onUpdatePlan={handleUpdatePlan}
-      />
-
-      {modalOpen && (
-        <ModalOverlay>
-          <ModalContent>
-            <h3>새 일정 추가</h3>
-            <form onSubmit={handleSubmit}>
-              <span>제목</span>
-              <Input
-                type="text"
-                name="title"
-                aria-label="제목"
-                required
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <span>설명</span>
-              <Textarea
-                name="description"
-                aria-label="설명"
-                required
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-              <span>시작 날짜</span>
-              <Input
-                type="datetime-local"
-                name="startDate"
-                aria-label="시작 날짜"
-                required
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-              <span>종료 날짜</span>
-              <Input
-                type="datetime-local"
-                name="endDate"
-                aria-label="종료 날짜"
-                required
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
-              <ToggleWrapper>
-                <ToggleLabel>공개 여부</ToggleLabel>
-                <ToggleInput
-                  type="checkbox"
-                  checked={isAccessible}
-                  onChange={() => setIsAccessible(!isAccessible)}
-                />
-              </ToggleWrapper>
-              <ToggleWrapper>
-                <ToggleLabel>완료 여부</ToggleLabel>
-                <ToggleInput
-                  type="checkbox"
-                  checked={isCompleted}
-                  onChange={() => setIsCompleted(!isCompleted)}
-                />
-              </ToggleWrapper>
-              <CloseButton type="submit">저장</CloseButton>
-            </form>
-          </ModalContent>
-        </ModalOverlay>
-      )}
-    </CalendarContainer>
+    <PageContainer>
+      <CustomCalendar plans={Plans} isReadOnly />
+      <Button
+        onClick={handleModifyClick}
+        theme="primary"
+        style={{ marginTop: "20px" }}
+      >
+        수정하기
+      </Button>
+    </PageContainer>
   );
-};
-
-export default MainPage;
+}
